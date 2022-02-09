4eb4662eeb9a22db4fce5ceba704ae242a29422b
parisasledge-source <parisasledge@me.com>
2022-02-09 10:56:29 -0500

    complete objective3lab1

--- Labs/Objective3Lab1.java
+++ Labs/Objective3Lab1.java
@@ -2,12 +2,12 @@ public class Objective3Lab1 {
   public static void main(String[] args) {
     double num1, num2, num3, average;
 
-    /*
-      Your solution goes here
-    */
     num1 = 15;
     num2 = 6;
     num3 = 2;
+    /*
+      Print out the calculate average of the numbers
+    */
     average = (num1 + num2 + num3)/3;
     System.out.println(average);
 
