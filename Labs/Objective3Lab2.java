c7bd704cd4dc60127bf98873af02e9c1517235c5
parisasledge-source <parisasledge@me.com>
2022-02-09 10:55:33 -0500

    complete objective3lab2

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -2,14 +2,13 @@ public class Objective3Lab2 {
   public static void main(String[] args) {
     double side1, side2, hypotenuse, num;
 
-    /*
-      Your solution goes here
-    */
     side1 = 10.0;
     side2 = 8.0;
     num = (side1*side1) + (side2*side2);
     hypotenuse = Math.sqrt(num);
-
+    /*
+      Print out the hypotenuse.
+    */
     System.out.println(hypotenuse);
 
   }
