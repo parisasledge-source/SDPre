f962cfa729f093313ff8f1bfdb458405205e11d6
parisasledge-source <parisasledge@me.com>
2022-02-09 18:24:06 -0500

    complete objective5lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,19 @@
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+    //If num1 is greater than num2
+    if(num1 > num2) {
+      //Print num1 is greater than num2
+      System.out.println(num1 + " is greater than " + num2);
+    }
+
+    //Else If num2 is greater than num1
+    else if(num2 > num1) {
+      //Print num2 is greater than num1
+      System.out.println(num2 + " is greater than " + num1);
+
+    }
+  }
+}
