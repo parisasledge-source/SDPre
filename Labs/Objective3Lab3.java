9fec24b09618dbf1fb8329389fb8bc6eb2ac0a40
parisasledge-source <parisasledge@me.com>
2022-02-09 10:20:19 -0500

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,16 @@
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+    int first, second, third, num;
+
+    /*
+      Your solution goes here
+    */
+    first = 1 + (int)(Math.random() * 6);
+    second = 1 + (int)(Math.random() * 6);
+    third = 1 + (int)(Math.random() * 6);
+    num = first + second + third;
+
+    System.out.println(first + " + " + second + " + " + third + " = " + num);
+
+  }
+}
