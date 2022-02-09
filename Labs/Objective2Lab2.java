fcdae13fb54516c696ed8444566013595cbe1e1c
parisasledge-source <parisasledge@me.com>
2022-02-08 20:06:02 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Parisa Sledge";
+    int age = 47;
+    String state = "Maryland";
+
+    /*
+      Your solution goes here
+    */
+    System.out.println("Name: "+name+" Age: "+age+" State: "+state);
+  }
+}
