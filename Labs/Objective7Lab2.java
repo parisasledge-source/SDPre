40451a8e297aaf013c9930baea629888069a212f
parisasledge-source <parisasledge@me.com>
2022-02-10 10:53:34 -0500

    complete objective7lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,12 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+  int counter = 0;
+
+    while(counter <= 20) {
+
+      System.out.println(counter); //Print the values 1 -> 20.
+
+      counter = counter + 1; //this makes we count forward
+    }
+  }
+}
