e36b542c722dcaa9f70eb1da3f3b6550a47bd1f4
parisasledge-source <parisasledge@me.com>
2022-02-10 10:36:33 -0500

    complete objective7lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,12 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+
+      System.out.println(counter); //Print the values 10 -> 1.
+
+      counter = counter - 1; //this makes we count down
+    }
+  }
+}
