64a23ed3f0ccbcaaffc5f764a49d90516de95086
parisasledge-source <parisasledge@me.com>
2022-02-10 18:58:54 -0500

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,15 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    //TODO Write a for loop that will print the numbers 1-10
+    for (int i = 0; i < 10; i++){
+      System.out.println(i+1);
+    }
+
+    //TODO Write another for loop that will print the numbers 10-1.
+    for (int i = 0; i < 10; i++){
+      System.out.println(10-i);
+    }
+
+  }
+}
