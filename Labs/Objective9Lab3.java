e9215caa7160dbfe633bd8c631c8ec5f0f1dae5d
parisasledge-source <parisasledge@me.com>
2022-02-10 21:01:31 -0500

    complete objective9lab3

---
+++ Labs/Objective9Lab3.java
@@ -0,0 +1,36 @@
+public class Objective9Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true){
+      //Call printMenu method.
+      printMenu();
+
+      selection = scanner.nextInt();
+
+      switch (selection) {
+        case '1':
+        System.out.println("Hello World");
+        break;
+        case '2':
+        System.out.println("Apple, Banana, Coconut");
+        break;
+        case '3':
+        System.out.println("Goodbye");
+        break;
+        default:
+    }
+
+    scanner.close();
+
+  }
+
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+}
