6e9a820b0697b4088798a1ccc3cfdb6b1a33ed13
parisasledge-source <parisasledge@me.com>
2022-02-10 20:28:54 -0500

    complete objective9lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,11 @@
+public class Objective9Lab1 {
+  public static void main(String[] args){
+  //Call printName method.
+  printName();
+
+  }
+  //printName method.
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
